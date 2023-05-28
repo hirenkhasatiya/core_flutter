c6db20339662397e200aa65c1f6f7e48d13fc933
hirenkhasatiya <khasatiyahiren43@gmail.com>
2023-05-28 21:47:55 +0530

    practical

---
+++ practical project/pr_2/lib/main.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Red & White"),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: "\t\t\t\t\t\t\t\t\t\t\tG",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "R",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "APHICS\n\n",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+
+                  ),
+                ),
+                TextSpan(
+                  text: "\t\t\t\tFLUTT",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "E",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "R\n\n",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "         AN",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "D",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "ROID\n\n",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: " DESIGN",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: " &",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: " DEVELOP\n\n",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "          W",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4.5,
+                  ),
+                ),
+                TextSpan(
+                  text: "EB\n\n",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "       FAS",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "H",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "ION\n\n",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "  ANIMAT",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.teal,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "I",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "ON\n\n",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.teal,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "           I",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "T",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "A-CS+\n\n",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "      GAM",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+                TextSpan(
+                  text: "E",
+                  style: TextStyle(
+                    fontSize: 23,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+    ),
+  );
+}
