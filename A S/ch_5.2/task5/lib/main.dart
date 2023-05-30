d5eac24d9b523ae61926322b57cfa5e5d71b35d6
hirenkhasatiya <khasatiyahiren43@gmail.com>
2023-05-30 11:53:35 +0530

    task

--- A S/ch_5.2/task5/lib/main.dart
+++ A S/ch_5.2/task5/lib/main.dart
@@ -5,37 +5,48 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor:  Colors.blue,
         appBar: AppBar(
-          backgroundColor: Colors.blue,
+          backgroundColor: Color(0xff182848),
           title: Text("An Indian Flag"),
           centerTitle: true,
         ),
-        body: Align(
-          alignment: Alignment.center,
-          child: Container(
-            height: 170,
-            width: 300,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.deepOrange.shade600,
-                  Colors.white,
-                  Colors.green.shade600,
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-              shape: BoxShape.rectangle,
-              border: Border.all(width: 1.5, color: Colors.white),
+        body: Ink(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xff182848),
+                Color(0xff4b6cb7),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
-            child: Center(
-              child: Text("✱", style:
-              TextStyle(
-                  color: Colors.blue.shade900,
+          ),
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 170,
+              width: 300,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.deepOrange.shade600,
+                    Colors.white,
+                    Colors.green.shade600,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+                shape: BoxShape.rectangle,
+                border: Border.all(width: 1.5, color: Colors.white),
+              ),
+              child: Center(
+                child: Text("✴️", style:
+                TextStyle(
+                  color: Colors.black,
                   fontWeight: FontWeight.w900,
                   fontSize: 30,
-              ),
+                ),
+                ),
               ),
             ),
           ),
